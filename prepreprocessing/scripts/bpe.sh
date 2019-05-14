68d35ed2733b49ab2e62cd6b084f139fcfc39b57
Margaret Chi <munchi@gmail.com>
2019-05-14 17:21:20 +1000

    Rename files created by BPE operations

--- prepreprocessing/scripts/bpe.sh
+++ prepreprocessing/scripts/bpe.sh
@@ -15,19 +15,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPT_DIR}/get_dirs.sh
 
 pushd ${DATA_DIR}
+joint_bpe_file=deen.bpe.$operations
 subword-nmt learn-joint-bpe-and-vocab -i train.truecased.de train.truecased.en \
-    --write-vocabulary vocab.de.$operations vocab.en.$operations -s $operations -o deen.bpe.$operations
+    --write-vocabulary vocab.de.$operations vocab.en.$operations -s $operations -o $joint_bpe_file
 
 for lang in de en
 do
     for corpus in train dev test
     do
         cased_file=$corpus.truecased.$lang
-        bped_file=$corpus.bpe.$lang
+        bped_file=$corpus.bpe.$operations.$lang
         if [ -f $cased_file ]
         then
             subword-nmt apply-bpe -i $cased_file -o $bped_file \
-                -c deen.bpe.$operations --vocabulary vocab.$lang --vocabulary-threshold 50
+                -c $joint_bpe_file --vocabulary vocab.$lang --vocabulary-threshold 50
         fi
     done
 done
